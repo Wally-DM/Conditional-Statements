73c5d0be20d0b26045abd72be4e2508dc22e90c8
David Wallace <dmwallace@usf.edu>
2021-09-15 20:18:31 -0500

    bruh

--- Program.cs
+++ Program.cs
@@ -6,7 +6,55 @@ namespace Conditional_Statements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); // change
+            // Ask the user for input
+            Console.WriteLine("Please enter the grade you expect to get in ISM 4300 ...");
+            /*
+              Use the try catch block to validate user input
+              If the user provides band input, the catch block
+            will handle the error and a message will be displayed.
+            */
+            try
+            {
+
+                // This variable will gather the user input
+                string input = Console.ReadLine();
+                /*
+                  This variable will  be used to store sales
+                after the input variable is parsed to decimal
+                */
+                decimal grades = decimal.Parse(input);
+
+                if (grades >= 90 & grades <= 100) 
+                {
+                    Console.WriteLine("Your expected grade is an A.");
+                    
+                }
+                else if (grades >= 80 & grades < 90)
+                {
+                    Console.WriteLine("Your expected grade is a B.");
+                }
+                else if (grades >= 70 & grades < 80)
+                {
+                    Console.WriteLine("Your expected grade is a C.");
+                }
+                else if (grades >= 60 & grades < 70)
+                {
+                    Console.WriteLine("Your expected grade is a D.");
+                }
+                else if (grades <= 50)
+                {
+                    Console.WriteLine("Your expected grade is a F.");
+                }
+                else
+                { 
+                }
+               
+                
+            }
+            catch
+            {
+
+            }
         }
     }
 }
